9fde6e058fb4367d16aef09f32879a9a5b2d20b0
Hannes Rüger <hannesrueger@gmx.de>
2021-07-12 19:44:34 +0200

    (api) fixed socket

--- api/src/index.ts
+++ api/src/index.ts
@@ -107,7 +107,7 @@ createConnection({
     }
 
     // That starts the server on the given port
-    app.listen(port, () => {
+    httpServer.listen(port, () => {
       // tslint:disable-next-line: no-console
       console.log(`Server started on port ${port}!`);
     });
